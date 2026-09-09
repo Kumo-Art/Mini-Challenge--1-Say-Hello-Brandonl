1cbadb21f93d615037f6b8a5289c1717f38d7095
Brandon Langehennig <brandonlangehennig67@gmail.com>
2026-09-09 12:06:07 -0700

    Peer review

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿//Brandon Langehennig
+//Mini challenge 1 Say Hello
+// I wrote a program that asks for user's name and then says hello and their name
+
+Console.WriteLine("Hello, what is your name?");
+
+string firstName = Console.ReadLine();
+
+Console.WriteLine("Hello," + firstName);
